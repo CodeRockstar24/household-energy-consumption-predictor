82c8db4cbefe626ca398ff86d2f7a69d1750bb59
Justin Elroy Silvere <153165728+JustinElroy123@users.noreply.github.com>
2025-09-23 23:46:54 +0400

    Update requirements and fix dependencies for Streamlit app

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import plotly as plt
 import plotly.graph_objects as go
 import plotly.express as px
-#import randomforestregressor
-from sklearn.ensemble import RandomForestRegressor
 
 
 # Load trained Random Forest model
